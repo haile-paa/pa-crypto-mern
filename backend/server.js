9023b9569cca53de4cedcfee2c4d02e9806e2919
haile-paa <haileyesuseyasu09@gmail.com>
2025-02-08 18:07:48 +0300

    mern-crypto

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { connectDB } = require("../db/db.js");
+const { connectDB } = require("./db/db.js");
 const {
   registerHandler,
   loginHandler,
